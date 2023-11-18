892d97290157bbe822bde882610c6b43b641f76e
ZweeDaLegend <91790736+ZweeDaLegend@users.noreply.github.com>
2023-11-18 15:51:37 +0100

    Update send.js

--- send.js
+++ send.js
@@ -1,19 +1,60 @@
 const axios = require('axios');
 const FormData = require('form-data');
 const fs = require('fs');
+const zipper = require('zip-local');
 
-const url = 'https://jth-udge2.hj.se/submit';
+/*
+  Settings you need to change
+*/
+
+const host ='cscx.org';
+const submitURL = 'https://cscx.org/submit';
 
 //Enter login
-const userName = 'username'
-const pwd = "password"
+const userName = 'username';
+const pwd = "password";
+
+/*
+  End of settings
+*/
 
 if (process.argv.length < 2) {
     console.log("enter problem name")
     return;
 }
+const problem = process.argv[2];
+
+if(fs.existsSync(problem+"/")){
+  zipper.sync.zip("./"+problem+"/").compress().save(problem+".zip");
+  fileName = problem+".zip";
+  console.log("Submitted a zip!")
+}
+else if(fs.existsSync(problem+".c")){
+  fileName = problem+".c";
+  console.log("Submitted a C source file!")
+}else{
+
+  console.log("Failed to find file to submit");
+  return;
+}
 
 const problem = process.argv[2];
+var fileName = ""; 
+
+if(fs.existsSync(problem+"/")){
+  zipper.sync.zip("./"+problem+"/").compress().save(problem+".zip");
+  fileName = problem+".zip";
+  console.log("Submitted a zip!")
+}
+else if(fs.existsSync(problem+".c")){
+  fileName = problem+".c";
+  console.log("Submitted a C source file!")
+}else{
+
+  console.log("Failed to find file to submit");
+  return;
+}
+
 
 const formData = new FormData();
 formData.append('user', userName);
@@ -25,13 +66,18 @@ formData.append('submit', 'Submit');
 
 const headers = {
   'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
-  'Host': 'jth-udge2.hj.se',
+  'Host': host,
   'Cache-Control': 'max-age=0',
 };
 
-axios.post(url, formData, { headers })
+axios.post(submitURL, formData, { headers })
   .then(response => {
-    console.log('Response:', response.data);
+    if(response.data.includes('Submission successful')){
+      console.log("Successfully submitted!")
+    }
+    else{
+      console.log('Response:', response.data);
+    }
 
 })
   .catch(error => {
